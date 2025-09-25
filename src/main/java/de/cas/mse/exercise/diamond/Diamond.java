85300cc1c4d4e8d6016139dae798ac4f05583f58
Paul Abel <59267403+Paul-Abel@users.noreply.github.com>
2025-09-25 13:19:53 +0000

    Gruppe 5

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,28 +4,30 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	public String print(int thickness) {
+		if (thickness <= 0 || (thickness % 2) == 0) {
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
+
+		StringBuilder builder = new StringBuilder(new String(fillRow(thickness, thickness)));
+		for (int spaces = thickness - 2; spaces > 0; spaces -= 2) {
+			char[] row = fillRow(thickness, spaces);
+			builder.insert(0, row);
+			builder.append(row);
 		}
 		return builder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	private char[] fillRow(int spaces, int stars) {
+		int starBegin = ((spaces - stars) / 2);
+		int starEnd = starBegin + stars;
+		char[] line = new char[starEnd + 1];
+		if (starBegin > 0) {
+			Arrays.fill(line, 0, starBegin, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		Arrays.fill(line, starBegin, starEnd, '*');
+		line[line.length - 1] = '\n';
+		return line;
 	}
 
 }
