f6c0956481cfcd76f56cfa8f9f106d1b4f20826c
adriankroeger <adriankroeger@gmx.de>
2025-09-25 13:17:30 +0000

    Use stringbuilder for row

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -1,32 +1,35 @@
 package de.cas.mse.exercise.diamond;
 
-import java.util.Arrays;
 
+// TODO make diamond factory 💸
 public class Diamond {
 
-	public String print(int numberOfRows) {
+	public String create(int numberOfRows) {
 		if (numberOfRows <= 0 || numberOfRows % 2 == 0) {
 			return "";
 		}
-		
-		StringBuilder builder = new StringBuilder(new String(makeRow(numberOfRows, numberOfRows)));
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(makeRow(numberOfRows, numberOfRows));
 		for (int numberOfStars = numberOfRows - 2; numberOfStars > 0; numberOfStars -= 2) {
-			char[] row = makeRow(numberOfRows, numberOfStars);
+			String row = makeRow(numberOfRows, numberOfStars);
 			builder.insert(0, row);
 			builder.append(row);
 		}
+
 		return builder.toString();
 	}
 
-	private char[] makeRow(int length, int numberOfStars) {
+	private String makeRow(int length, int numberOfStars) {
 		int leftPadding = ((length - numberOfStars) / 2);
-		char[] row = new char[leftPadding + numberOfStars + 1];
+		StringBuilder builder = new StringBuilder(leftPadding + numberOfStars + 1);
 		if (leftPadding > 0) {
-			Arrays.fill(row, 0, leftPadding, ' ');
+			builder.append(" ".repeat(leftPadding));
 		}
-		Arrays.fill(row, leftPadding, leftPadding + numberOfStars, '*');
-		row[row.length - 1] = '\n';
-		return row;
+		
+		builder.append("*".repeat(numberOfStars));
+		builder.append("\n");
+		return builder.toString();
 	}
 
 }
