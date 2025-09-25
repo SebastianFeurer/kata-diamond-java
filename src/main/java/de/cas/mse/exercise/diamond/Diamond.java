112c408b01b7be5f56f95273483c0943abcac3f8
Branchlord <Leurat.Zabergja@cas.de>
2025-09-25 13:23:53 +0000

    Gruppe 3

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,26 +4,34 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	private final static int PREPEND_OFFSET = 0;
+	private final static int NUMBER_OF_STARS_TO_REMOVE_PER_LINE = 2;
+
+	public String print(int rowCount) {
+		if (validateRowCount(rowCount)) {
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
+		StringBuilder builder = new StringBuilder();
+		builder.append(createLine(rowCount, rowCount));
+		for (int currentRow = rowCount - NUMBER_OF_STARS_TO_REMOVE_PER_LINE; currentRow > 0; currentRow -= NUMBER_OF_STARS_TO_REMOVE_PER_LINE) {
+			char[] chars = createLine(rowCount, currentRow);
+			builder.insert(PREPEND_OFFSET, chars);
 			builder.append(chars);
 		}
 		return builder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	private boolean validateRowCount(int rowCount){
+		return rowCount <= 0 || rowCount % 2 == 0;
+	}
+
+	private char[] createLine(int maxRowCount, int currrentRowCount) {
+		int amountOfSpaces = ((maxRowCount - currrentRowCount) / 2);
+		char[] chars = new char[amountOfSpaces + currrentRowCount + 1];
+		if (amountOfSpaces > 0) {
+			Arrays.fill(chars, 0, amountOfSpaces, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
+		Arrays.fill(chars, amountOfSpaces, amountOfSpaces + currrentRowCount, '*');
 		chars[chars.length - 1] = '\n';
 		return chars;
 	}
